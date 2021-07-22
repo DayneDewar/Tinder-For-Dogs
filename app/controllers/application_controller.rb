fee41c4e07890df25e8a028e3c6f04fcea4f2a8d
Dayne Dewar <daynedewar@yahoo.com>
2021-07-22 12:57:46 -0400

    added a few accounts and upddated devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
     private
 
     def configure_devise
-        devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :dog_name, :username, images: [] ])
+        devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :dog_name, images: [] ])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :dog_name, :username, images: [] ])
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-accounts = [
-  {email: "dayne@example.com", first_name: "Dayne", dog_name: "Sora", username: "dayne1", password: "Password1"},
-  {email: "chris@example.com", first_name: "Chris", dog_name: "Stormi", username: "chris1", password: "Password1"},
-  {email: "andrea@example.com", first_name: "Andrea", dog_name: "Kona", username: "andrea1", password: "Password1"},
-]
+# accounts = [
+#   {email: "dayne@example.com", first_name: "Dayne", dog_name: "Sora", username: "dayne1", password: "Password1"},
+#   {email: "chris@example.com", first_name: "Chris", dog_name: "Stormi", username: "chris1", password: "Password1"},
+#   {email: "andrea@example.com", first_name: "Andrea", dog_name: "Kona", username: "andrea1", password: "Password1"},
+# ]
 
-accounts.each do |acc|
-    Account.create(acc)
-end
+# accounts.each do |acc|
+#     Account.create(acc)
+# end
 
-puts "seeded & beaded"
+# puts "seeded & beaded"
