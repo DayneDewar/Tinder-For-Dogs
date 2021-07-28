8a0667d5362aa1cd031fa0b68a345ebb9f52b99b
Dayne Dewar <daynedewar@yahoo.com>
2021-07-28 09:57:20 -0500

    added Like model and refined browse controller on approval

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,10 +1,23 @@
 class BrowseController < ApplicationController
     def browse
-        @dogs = Account.where.not(id: current_account.id)
+        liked_account_ids = Like.where(account_id: current_account.id).map(&:liked_account_id)
+        liked_account_ids << current_account.id
+        @dogs = Account.where.not(id: liked_account_ids)
     end
 
     def approve
-        logger.debug "User id is matching #{params[:id]}"
+        account_id = params[:id]
+        logger.debug "User id is matching #{account_id}"
+
+        new_like = Like.new(liked_account_id: account_id)
+        new_like.account_id = current_account.id
+
+        if new_like.save
+            Like.where(account_id: account_id, liked_account_id: current_account.id)
+            @they_like_us = existing_like > 0
+        else
+
+        end
     end
 
     def decline
